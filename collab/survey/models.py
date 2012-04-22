712c961e87666bc084d3a21e73a4c0d84a004e56
Peter Lam <peterlam26@hotmail.com>
2012-04-22 13:48:10 -0700

    adding question

--- collab/survey/models.py
+++ collab/survey/models.py
@@ -1,10 +1,18 @@
+import datetime
 from django.db import models
 
 class Survey(models.Model):
     question = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    def __unicode__(self):
+        return self.question
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     survey = models.ForeignKey(Survey)
     choice = models.CharField(max_length=200)
     votes = models.IntegerField()
+    def __unicode__(self):
+        return self.choice
