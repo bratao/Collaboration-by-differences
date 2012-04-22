b8ccd101bc9b6f1d0c64be1648594534b5201299
Peter Lam <peterlam26@hotmail.com>
2012-04-22 15:09:41 -0700

    Adding URL structure

--- collab/collab/urls.py
+++ collab/collab/urls.py
@@ -6,8 +6,14 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'collab.views.home', name='home'),
-    # url(r'^collab/', include('collab.foo.urls')),
+    #url(r'^$', 'collab.views.home', name='home'),
+    #url(r'^collab/', include('collab.foo.urls')),
+    url(r'^survey/$', 'survey.views.index'),
+    url(r'^survey/(?P<survey_id>\d+)/$', 'survey.views.detail'),
+    url(r'^survey/(?P<survey_id>\d+)/results/$', 'survey.views.results'),
+    url(r'^survey/(?P<survey_id>\d+)/vote/$', 'survey.views.vote'),
+    url(r'^admin/', include(admin.site.urls)),
+
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
--- collab/survey/views.py
+++ collab/survey/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the survey index.")
+
+def detail(request, survey_id):
+    return HttpResponse("You're looking at survey %s." % survey_id)
+
+def results(request, survey_id):
+    return HttpResponse("You're looking at the results of survey %s." % survey_id)
+
+def vote(request, survey_id):
+    return HttpResponse("You're voting on survey %s." % survey_id)
