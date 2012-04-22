0de90165af7bcf4078b97568b2d07a90dc13a918
bratao <brataoufba@gmail.com>
2012-04-22 14:43:52 -0700

    Register surveys

--- collab/survey/admin.py
+++ collab/survey/admin.py
@@ -1,5 +1,18 @@
-from surveys.models import Survey
+from survey.models import Survey, Choice
 from django.contrib import admin
 
-admin.site.register(Survey)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
 
+class SurveyAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question', 'pub_date')
+    search_fields = ['question']
+    #list_filter = ['pub_date']
+
+admin.site.register(Survey, SurveyAdmin)
