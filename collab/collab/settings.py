74d25ddece52039fcd0472f9b28c2e5ef634fe3e
bratao <brataoufba@gmail.com>
2012-04-22 15:28:23 -0700

    Survey template index

--- collab/collab/settings.py
+++ collab/collab/settings.py
@@ -106,6 +106,7 @@ ROOT_URLCONF = 'collab.urls'
 WSGI_APPLICATION = 'collab.wsgi.application'
 
 TEMPLATE_DIRS = (
+    "templates"
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
--- collab/survey/views.py
+++ collab/survey/views.py
@@ -1,7 +1,10 @@
+from django.shortcuts import render_to_response
+from survey.models import Survey
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the survey index.")
+    latest_survey_list = Survey.objects.all().order_by('-pub_date')[:5]
+    return render_to_response('survey/index.html', {'latest_survey_list': latest_survey_list})
 
 def detail(request, survey_id):
     return HttpResponse("You're looking at survey %s." % survey_id)
