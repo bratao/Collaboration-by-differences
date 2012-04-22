3f0b7ee4280d1160b2acf791897782cd14b9d6f9
bratao <brataoufba@gmail.com>
2012-04-22 15:52:59 -0700

    Rest of the templates

--- collab/survey/views.py
+++ collab/survey/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render_to_response
-from survey.models import Survey
-from django.http import HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404
+from survey.models import Survey, Choice
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.template import RequestContext
+
 
 
 def index(request):
@@ -10,11 +12,28 @@ def index(request):
 
 def detail(request, survey_id):
     s = get_object_or_404(Survey, pk=survey_id)
-    return render_to_response('survey/detail.html', {'survey': s})
+    return render_to_response('survey/detail.html', {'survey': s},
+                               context_instance=RequestContext(request))
 
 
 def results(request, survey_id):
-    return HttpResponse("You're looking at the results of survey %s." % survey_id)
+    s = get_object_or_404(Survey, pk=survey_id)
+    return render_to_response('survey/results.html', {'survey': s})
 
 def vote(request, survey_id):
-    return HttpResponse("You're voting on survey %s." % survey_id)
+    s = get_object_or_404(Survey, pk=survey_id)
+    try:
+        selected_choice = s.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the survey voting form.
+        return render_to_response('survey/detail.html', {
+            'survey': s,
+            'error_message': "You didn't select a choice.",
+        }, context_instance=RequestContext(request))
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('survey.views.results', args=(s.id,)))
